58c2156adeda8fdee22dda1c828cb5214b5913ba
DuoThareendra <thareendra@duosoftware.com>
2025-04-18 21:26:37 +0530

    Initial Commit

---
+++ code/tracktern/tracktern/trackternApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackternApp.swift
+//  tracktern
+//
+//  Created by Thareendra Tennakoon on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct trackternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code/tracktern/trackternTests/trackternTests.swift
@@ -0,0 +1,17 @@
+//
+//  trackternTests.swift
+//  trackternTests
+//
+//  Created by Thareendra Tennakoon on 4/18/25.
+//
+
+import Testing
+@testable import tracktern
+
+struct trackternTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
